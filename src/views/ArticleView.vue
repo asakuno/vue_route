bed7228073f6e093dcba373d1355e8a8789fd159
asakuno <takahiro.asakuno@gmail.com>
2023-01-14 22:35:55 +0900

    [Add]vue_router

---
+++ src/views/ArticleView.vue
@@ -0,0 +1,15 @@
+<template>
+  <span>記事コード: {{  $route.params.aid }}</span>
+</template>
+
+<script>
+export default {
+  name: 'ArticleView',
+  props: {
+    aid: String
+  }
+  // props: {
+  //   aid: Number
+  // }
+}
+</script>
